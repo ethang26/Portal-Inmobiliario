cb1eaba729c3553b22632c35291db6d1648cf0e4
ethang26 <ethancusi2005@gmail.com>
2025-09-26 20:20:19 -0500

    Primer commit - proyecto base .NET

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Portal_Inmobiliario.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
