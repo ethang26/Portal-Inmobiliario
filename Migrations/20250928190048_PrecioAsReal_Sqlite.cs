2c30d9bf940b6900d4281be7be685184736971e0
ethang26 <ethancusi2005@gmail.com>
2025-09-28 16:37:11 -0500

    tablas render

---
+++ Migrations/20250928190048_PrecioAsReal_Sqlite.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portal_Inmobiliario.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrecioAsReal_Sqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
